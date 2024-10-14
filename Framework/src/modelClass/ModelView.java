323be509023384c9ce498fc3f1f69df01fc9c6c5
NA-Anthony <159878489+NA-Anthony@users.noreply.github.com>
2024-10-14 09:24:34 +0300

    sprint4

---
+++ Framework/src/modelClass/ModelView.java
@@ -0,0 +1,41 @@
+package modelClass;
+
+import java.util.HashMap;
+
+public class ModelView {
+    private String url;
+    private HashMap<String, Object> data = new HashMap<>();   
+    
+    public ModelView(String url, HashMap<String, Object> data) {
+        this.url = url;
+        this.data = data;
+    }
+
+    public ModelView() {
+    }
+
+    public ModelView(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+    
+    // Fonction addObject pour ajouter des données au HashMap
+    public void addObject(String key, Object value) {
+        data.put(key, value);
+    }
+}
